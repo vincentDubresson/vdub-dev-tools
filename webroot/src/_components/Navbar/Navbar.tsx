90f2d6c70f8ddd63038ba43ed8700d4944cd9e85
vincentDubresson <vincent.dubresson@orange.fr>
2023-06-04 23:11:45 +0200

    Manage responsive layout.

---
+++ webroot/src/_components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import styles from './Navbar.module.scss';
+
+const Navbar = () => {
+  return (
+    <aside className={styles.aside}>
+      <h1 className={styles.title}>Hello</h1>
+    </aside>
+  );
+};
+
+export default Navbar;
