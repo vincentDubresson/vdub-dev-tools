61c9b205fbbc039150f2d1c94252dceec7c6db56
vincentDubresson <vincent.dubresson@orange.fr>
2023-06-09 16:05:46 +0200

    add theme mode toggle button and home button in navbar

--- webroot/src/_components/Navbar/Navbar.tsx
+++ webroot/src/_components/Navbar/Navbar.tsx
@@ -1,12 +1,23 @@
 import styles from './Navbar.module.scss';
 import logo from '../../../public/logo.svg';
 import Image from 'next/image';
+import ThemeToggleButton from '../ThemeToggleButton/ThemeToggleButton';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import Link from 'next/link';
 
-const Navbar = () => {
+const Navbar = ({ handleThemeToggle }: { handleThemeToggle: any }) => {
+  const handleThemeToggleClick = (themeMode: 'light' | 'dark') => {
+    handleThemeToggle(themeMode);
+  };
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo_container}>
+        <Link href="/" className={styles.button}>
+          <HomeRoundedIcon />
+        </Link>
         <Image className={styles.logo} src={logo} alt="logo" />
+        <ThemeToggleButton themeToggle={handleThemeToggleClick} />
       </div>
       <div className={styles.menu}></div>
     </nav>
---
+++ webroot/src/_components/ThemeToggleButton/ThemeToggleButton.tsx
@@ -0,0 +1,30 @@
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import NightlightOutlinedIcon from '@mui/icons-material/NightlightOutlined';
+import styles from './ThemeToggleButton.module.scss';
+import { useState } from 'react';
+
+const ThemeToggleButton = ({ themeToggle }: { themeToggle: any }) => {
+  const [theme, setTheme] = useState<'light' | 'dark'>('light');
+
+  const handleThemeToggleClick = (themeMode: 'light' | 'dark') => {
+    setTheme(themeMode);
+    themeToggle(themeMode);
+  };
+
+  return (
+    <button
+      className={styles.button}
+      onClick={() =>
+        handleThemeToggleClick(theme === 'light' ? 'dark' : 'light')
+      }
+    >
+      {theme === 'light' ? (
+        <WbSunnyIcon />
+      ) : (
+        <NightlightOutlinedIcon sx={{ color: 'white' }} />
+      )}
+    </button>
+  );
+};
+
+export default ThemeToggleButton;
