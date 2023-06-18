e90243eb65231c28cbb974e7b9688db64656d4f3
vincentDubresson <vincent.dubresson@orange.fr>
2023-06-18 21:05:42 +0200

    Reformat css of home page and buttons

--- webroot/src/app/page.tsx
+++ webroot/src/app/page.tsx
@@ -3,7 +3,7 @@ import './globals.scss';
 import styles from './page.module.scss';
 import logo from '../../public/logo.svg';
 import Image from 'next/image';
-import ImageIcon from '@mui/icons-material/Image';
+import AspectRatioOutlinedIcon from '@mui/icons-material/AspectRatioOutlined';
 import Link from 'next/link';
 
 export default function Home() {
@@ -17,59 +17,12 @@ export default function Home() {
       </header>
       <section className={styles.button_container}>
         <Link href="/placeholder" className={styles.link}>
-          <ImageIcon />
-          Générateur de placeholder
-        </Link>
-        <Link href="/" className={styles.link}>
-          En construction
-        </Link>
-        <Link href="/" className={styles.link}>
-          En construction
-        </Link>
-        <Link href="/" className={styles.link}>
-          En construction
-        </Link>
-        <Link href="/" className={styles.link}>
-          En construction
-        </Link>
-        <Link href="/" className={styles.link}>
-          En construction
-        </Link>
-        <Link href="/" className={styles.link}>
-          En construction
-        </Link>
-        <Link href="/" className={styles.link}>
-          En construction
-        </Link>
-        <Link href="/" className={styles.link}>
-          En construction
-        </Link>
-        <Link href="/" className={styles.link}>
-          En construction
-        </Link>
-        <Link href="/" className={styles.link}>
-          En construction
-        </Link>
-        <Link href="/" className={styles.link}>
-          En construction
-        </Link>
-        <Link href="/" className={styles.link}>
-          En construction
-        </Link>
-        <Link href="/" className={styles.link}>
-          En construction
-        </Link>
-        <Link href="/" className={styles.link}>
-          En construction
-        </Link>
-        <Link href="/" className={styles.link}>
-          En construction
-        </Link>
-        <Link href="/" className={styles.link}>
-          En construction
-        </Link>
-        <Link href="/" className={styles.link}>
-          En construction
+          <AspectRatioOutlinedIcon />
+          <h2>Générateur de placeholder</h2>
+          <p className={styles.description}>
+            Personnalisez la taille, le type de fichier et les couleurs pour
+            obtenir des espaces réservés adaptés à vos besoins de conception.
+          </p>
         </Link>
       </section>
     </main>
